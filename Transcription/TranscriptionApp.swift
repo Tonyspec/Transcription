3b1e484f87edc02d99e299b365f858e0101e6978
Anton Achedžak <anton.achedzak@gmail.com>
2025-01-24 13:47:43 +0100

    Initial Commit

---
+++ Transcription/TranscriptionApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranscriptionApp.swift
+//  Transcription
+//
+//  Created by Anton Achedžak on 24.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct TranscriptionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TranscriptionTests/TranscriptionTests.swift
@@ -0,0 +1,17 @@
+//
+//  TranscriptionTests.swift
+//  TranscriptionTests
+//
+//  Created by Anton Achedžak on 24.01.2025.
+//
+
+import Testing
+@testable import Transcription
+
+struct TranscriptionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
